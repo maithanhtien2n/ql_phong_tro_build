86632b4e000d44b36490a5e0f7ca7fe60c387b4f
Tiện Thanh <tienmt@imt-soft.com>
2023-09-08 15:16:59 +0700

    build

---
+++ assets/Home-93c0d206.js
@@ -0,0 +1,1 @@
+import{o as e,c as t,b as s}from"./index-29c451c3.js";const o="/assets/banner-2ae4175c.png",c={class:"h-full flex justify-content-center align-items-center"},n=s("img",{class:"w-25rem",src:o,alt:""},null,-1),a=[n],m={__name:"Home",setup(l){return console.log(localStorage.getItem("token")),(r,_)=>(e(),t("div",c,a))}};export{m as default};
